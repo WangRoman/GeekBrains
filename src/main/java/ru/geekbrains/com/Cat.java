af37a4816cb97d5e5b25f0a94af5e5d98f57240c
Roman <christmasspb@gmail.com>
2024-06-13 14:23:11 +0300

    Lesson-1-7 has been added

--- src/main/java/ru/geekbrains/com/Cat.java
+++ src/main/java/ru/geekbrains/com/Cat.java
@@ -1,27 +1,57 @@
 package ru.geekbrains.com;
 
-public class Cat extends Animal {
+import java.io.IOException;
 
-    @Override
-    public void run(int length) {
-        if (length <= 200) {
-            System.out.println("Run");
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean fullness;
+
+    public Cat(String name, int appetite, boolean fullness) {
+        this.name = name;
+        this.appetite = appetite;
+        this.fullness = fullness;
+    }
+
+
+    public void eat(Plate plate, int food) throws IOException {
+        if (plate.getFood() >= appetite) {
+            plate.decreaseFood(appetite, name);
+            appetite = 0;
+            fullness = true;
         } else {
-            System.out.println("Cat can't run such a distance");
+            System.out.println("It's not enough for " + name + ", that's why he doesn't eat");
+            plate.addFood();
         }
     }
 
-    @Override
-    public void swim(int length) {
-        System.out.println("Cat can't swim");
+    public boolean fullness() {
+        return appetite == 0;
     }
 
-    @Override
-    public void jump(double height) {
-        if (height <= 2) {
-            System.out.println("Jump");
-        } else {
-            System.out.println("Cat can't jump so high");
-        }
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+
+    public boolean getFullness() {
+        return fullness;
+    }
+
+    public void setFullness(boolean fullness) {
+        this.fullness = fullness;
+    }
+
+
 }
--- src/main/java/ru/geekbrains/com/Main.java
+++ src/main/java/ru/geekbrains/com/Main.java
@@ -1,16 +1,30 @@
 package ru.geekbrains.com;
 
-public class Main {
-    public static void main(String[] args) {
-        Animal animal = new Animal();
-        Animal cat = new Cat();
-        Animal dog = new Dog();
-        cat.run(50);
-        dog.run(600);
-        cat.swim(30);
-        dog.swim(5);
-        cat.jump(2);
-        dog.jump(0.7);
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int foodCount = 40;
+        Cat [] cat = new Cat [3]; {
+            cat[0] = new Cat("Barsik", 15, false);
+            cat[1] = new Cat("Murzik", 15, false);
+            cat[2] = new Cat("Vaska", 15, false);
+        }
+        Plate plate = new Plate(foodCount);
+        while (cat[2].getAppetite() != 0) {
+            plate.info();
+            System.out.println("Аппетит у кота " + cat[0].getName() + ": " + cat[0].getAppetite());
+            System.out.println("Сытость у кота " + cat[0].getName() + ": " + cat[0].fullness());
+            cat[0].eat(plate, foodCount);
+            plate.info();
+            System.out.println("Аппетит у кота " + cat[1].getName() + ": " + cat[1].getAppetite());
+            System.out.println("Сытость у кота " + cat[1].getName() + ": " + cat[1].fullness());
+            cat[1].eat(plate, foodCount);
+            plate.info();
+            System.out.println("Аппетит у кота " + cat[2].getName() + ": " + cat[2].getAppetite());
+            System.out.println("Сытость у кота " + cat[2].getName() + ": " + cat[2].fullness());
+            cat[2].eat(plate, foodCount);
+            plate.info();
+        }
     }
 }
